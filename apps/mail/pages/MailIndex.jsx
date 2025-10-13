6d789438c65687c106fa0588a41d6b19318ffe1a
Yoav Natan <yoavnatan.yn@gmail.com>
2025-10-13 16:37:21 +0300

    Add unread mails counter

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -33,18 +33,33 @@ export function MailIndex() {
 
     function onReadMail(mail) {
         mailService.readMail(mail)
+        setMails(mails)
     }
 
     function onSetFilterBy(filterByToEdit) {
         setFilterBy(prevFilter => ({ ...prevFilter, ...filterByToEdit }))
     }
 
+    function countUnreadMails() {
+        let count = 0
+        mails.forEach(mail => {
+            if (!mail.isRead) count++
+        })
+        return count
+    }
+
     if (!mails) return <div className="loader">Loading...</div>
     return (
         <section className="mail-index">
             <MailFilter onSetFilterBy={onSetFilterBy} filterBy={filterBy} />
-            <section className="mails-container">
-                <MailList mails={mails} onReadMail={onReadMail} />
+            <section className="mail-index inner-container flex">
+                <div className="mail-folders container">
+                    <div className="roboto-bold mail-folder inbox">Inbox {countUnreadMails()}</div>
+                    <div className="roboto-bold mail-folder inbox">trash </div>
+                </div>
+                <section className="mails-container">
+                    <MailList mails={mails} onReadMail={onReadMail} />
+                </section>
             </section>
         </section>
     )
