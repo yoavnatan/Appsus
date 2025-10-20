94ebf9c49bf2755f9547ecfb72925280d50200bf
liaddj <liad.7735@gmail.com>
2025-10-20 19:02:09 +0300

    Add roboto font

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -8,17 +8,11 @@ export function NoteList({notes, onRemoveNote, saveNote}) {
 
  if (!notes || !notes.length) return <h2>No notes to show...</h2>
 
-
-
-
-
-
-
     return (
         <ul className="note-list">
-            {notes.map((note,num) => (
-                <li key={note.id} className={"note-preview-container" + num}>
-                    <NotePreview note={note} noteNum={num} onRemoveNote={onRemoveNote} saveNote={saveNote} />
+            {notes.map((note) => (
+                <li key={note.id} className="note-preview-container">
+                    <NotePreview note={note}  onRemoveNote={onRemoveNote} saveNote={saveNote} />
                 </li>
             ))}
         </ul>
